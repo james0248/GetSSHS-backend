7cef310ea79191d36440f66696bb3282b6c0cde9
james0248 <hsjames0248@gmail.com>
2020-03-28 02:42:58 +0900

    Only send score and name

--- src/api/ranking.js
+++ src/api/ranking.js
@@ -1,7 +1,14 @@
 module.exports = async (ctx, next) => {
     const ranking = ctx.state.collection.ranking
     const leaderBoard = await ranking.find().sort({ score: -1 }).limit(10).toArray()
+    let scores = leaderBoard.map(info => {
+        let newInfo = {
+            name: info.name,
+            score: info.score,
+        }
+        return newInfo
+    })
     
     ctx.response.status = 200
-    ctx.response.body = leaderBoard
+    ctx.response.body = scores
 }
